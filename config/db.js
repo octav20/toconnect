f6d0680933ffe6af3579803c2cd5d147bab3b6a4
octav20 <octav20>
2023-03-18 01:08:23 -0700

    configuracion de la base de datos

---
+++ config/db.js
@@ -0,0 +1,135 @@
+const { Sequelize, DataTypes } = require('sequelize');
+import * as dotenv from 'dotenv'
+dotenv.config()
+/* Creating a new instance of Sequelize. */
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: {
+        freezeTableName: true,
+    }
+});
+
+export const user = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    online: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+    },
+
+}, {
+    timestamps: false
+})
+
+export const room = sequelize.define('room', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    type_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+
+}, {
+    timestamps: false
+})
+
+export const typeMessage = sequelize.define('type_message', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+
+}, {
+    timestamps: false
+})
+
+export const roomType = sequelize.define('room_type', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+    timestamps: false
+})
+
+export const userRoom = sequelize.define('user_room', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    room_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+
+}, {
+    timestamps: false
+})
+
+export const message = sequelize.define('message', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    room_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    type_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    url: {
+        type: DataTypes.STRING
+    }
+
+}, {
+    timestamps: false
+})
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { sequelize, user, room, roomType, message, typeMessage, userRoom } from "../../config/db"
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  try {
+
+
+    await sequelize.authenticate();
+
+    console.log('Connection has been established successfully.');
+    (async () => {
+      await sequelize.sync({ alter: true });
+      // const jane = user.build({ username: "juan", password: "1234" });
+      const userCreated = await user.create({ username: "tony", password: "1234" });
+      // await jane.save();
+      // console.log(jane.toJSON().online);
+      // console.log('juan was saved to the database!');
+      // const userRooms = await userRoom.findAll();
+      console.log(JSON.stringify(userCreated, null, 2));
+    })();
+    console.log(process.env.DB_NAME);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
   res.status(200).json({ name: 'John Doe' })
 }
